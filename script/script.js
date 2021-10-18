68a83319aaee28b34e2cf66f233caf548b2a622a
FranGuerre <franguerrero054@gmail.com>
2021-10-18 12:21:10 -0300

    agregar tern.js a ignore y emprolijar JS

--- script/script.js
+++ script/script.js
@@ -9,17 +9,15 @@ let contenido_2 = document.getElementById("contenido-2");
 function menuScroll() {
   menu_scroll.forEach((menu, i) => {
     menu.addEventListener("click",(e) => {
-      if(e.target.classList.contains("menu-scroll__opcion")) {
-        let menu_id = e.target.parentElement.id;
-        let child_n = Array.prototype.indexOf.call(e.target.parentNode.children, e.target);
-        console.log(child_n);
+      if(e.target.classList.contains("menu-scroll__opcion"))
+        let menu_activo = e.target.parentElement;
+        // menu_id obtiene del id que menu está siendo utilizado
+        let menu_id = menu_activo.id;
         menu_id = menu_id[menu_id.length - 1];
+        // child_index obtiene el índice del child para luego utilizarlo para modificar la posición
+        let child_index = Array.prototype.indexOf.call(menu_activo.children, e.target);
 
-        document.getElementById(`contenido-${menu_id}`).style.left = ((child_n) * -360) + "px";
-        console.log(document.getElementById(`contenido-${menu_id}`).style.left);
-
-
-
+        document.getElementById(`contenido-${menu_id}`).style.left = ((child_index) * -360) + "px";
       }
     });
   });
